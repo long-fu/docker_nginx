ec6f2ab5ed2131de570c0c41ef3f4e972267a939
longfu <haoshuai9291@163.com>
2023-11-21 11:30:51 +0800

    测试运行成功

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # docker run -it --rm -p 80:80 -p 8080:8080 -p 1935:1935 nginx:v1.22.1 bash
 
-docker run -d -p 80:80 -p 8080:8080 -p 1935:1935 nginx:v1.22.1
+# docker run -d -p 80:80 -p 8080:8080 -p 1935:1935 nginx:v1.22.1
+
+# docker run -it --rm --net=host nginx:v1.22.1

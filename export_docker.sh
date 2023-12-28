7a4bfbee8afb9211fb9d8ecb8a55ead5622d6bcf
longfu <haoshuai9291@163.com>
2023-12-28 13:37:28 +0800

    测试脚本 导出平台限制

--- export_docker.sh
+++ export_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker save nginx:v1.22.1 > nginx-1_22_1.tar
+docker save nginx:v1.22.1 > nginx-1_22_1_$(arch).tar
+
+# docker save nginx:v1.22.1 > nginx-1_22_1_x86_64.tar
--- run.sh
+++ run.sh
@@ -4,3 +4,14 @@ echo "$1" | sudo -S docker run -d --restart always --name nginx \
  --net=host \
  -v /home/azurengine/workspace/html:/opt/nginx/html \
  nginx:v1.22.1
+
+docker run -d --restart always --name nginx \
+ --net=host \
+ -v $(pwd)/html:/opt/nginx/html \
+ nginx:v1.22.1 
+
+docker run -it --rm --name nginx \
+ --net=host \
+ -v $(pwd)/html:/opt/nginx/html \
+ nginx:v1.22.1 \
+ /bin/bash
